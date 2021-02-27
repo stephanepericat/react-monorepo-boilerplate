7ed58f50bd66a976841e7d391cc965fde253c127
Stephane P. Pericat <contact@pericat.net>
2021-02-27 12:25:27 -0600

    build components

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,8 @@ module.exports = {
     "env": {
         "browser": true,
         "es2021": true,
-        "jest/globals": true
+        "jest/globals": true,
+        "node": true
     },
     "extends": [
         "eslint:recommended",
---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  mode: "production",
+  module: {
+    rules: [
+      {
+        test: /.jsx?/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react"
+            ]
+          }
+        }
+      }
+    ]
+  }
+}
