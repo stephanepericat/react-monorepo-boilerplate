799babf1fdee6d73388ae751e5892fd3e5f5c934
Stephane P. Pericat <contact@pericat.net>
2021-02-27 10:34:11 -0600

    example styled components

---
+++ packages/demo/button/src/button.stories.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { Button } from './index';
+
+export default {
+  title: 'Boilerplate/Button',
+  component: Button,
+};
+
+const Template = (args) => <Button {...args}>{args.label}</Button>;
+
+export const Default = Template.bind({});
+Default.args = {
+  label: "click me",
+  primary: true
+};
---
+++ packages/demo/button/src/index.js
@@ -0,0 +1,23 @@
+// import React from 'react';
+import styled, { css } from 'styled-components';
+
+export const Button = styled.a`
+  display: inline-block;
+  border-radius: 3px;
+  padding: 0.5rem 0;
+  margin: 0.5rem 1rem;
+  width: 11rem;
+  background: transparent;
+  color: black;
+  border: 2px solid black;
+  text-align: center;
+
+  /* The GitHub button is a primary button
+   * edit this to target it specifically! */
+  ${props => props.primary && css`
+    background: orange;
+    color: white;
+    border-color: orange;
+  `}
+`;
+
