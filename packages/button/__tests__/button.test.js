9f0e5e78a9ef6d2943cfc47551d8ae79107d3c76
Stephane P. Pericat <contact@pericat.net>
2021-02-27 11:21:02 -0600

    proper test for button

--- packages/button/__tests__/button.test.js
+++ packages/button/__tests__/button.test.js
@@ -1,5 +1,14 @@
+import React from "react";
+import renderer from 'react-test-renderer';
+import { Button } from "../src/index";
+import "jest-styled-components";
+
 describe("Button", () => {
-  it("should work", () => {
-    expect(true).toBe(true);
+  it("should render an anchor", () => {
+    const component = renderer.create(<Button>Click Me</Button>);
+    const tree = component.toJSON();
+
+    expect(tree.type).toEqual('a');
+    expect(tree.children).toEqual(['Click Me']);
   });
 });
