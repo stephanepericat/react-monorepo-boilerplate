de85048941604c624f4ab4a8a4bf46caa1aed5f1
Stephane P. Pericat <contact@pericat.net>
2021-02-27 11:37:05 -0600

    test props for button component

--- packages/button/__tests__/button.test.js
+++ packages/button/__tests__/button.test.js
@@ -11,4 +11,12 @@ describe("Button", () => {
     expect(tree.type).toEqual("a");
     expect(tree.children).toEqual(["Click Me"]);
   });
+
+  it("should style a primary button differently", () => {
+    const args = { primary: true };
+    const component = renderer.create(<Button {...args}>Click Me</Button>);
+    const tree = component.toJSON();
+
+    expect(tree).toHaveStyleRule('background', 'orange');
+  });
 });
